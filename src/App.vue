7354a45dfc96a17afdc591f9eb6e011c6c8b2e05
Juan Jose Lopez <juanjose.lopez@edreamsodigeo.com>
2024-05-26 22:30:16 +0200

    chore: test gh-pages

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { useRoute } from 'vue-router'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  setup() {
+    const route = useRoute()
+
+    return {
+      currentRoute: route
+    }
   }
 }
 </script>
