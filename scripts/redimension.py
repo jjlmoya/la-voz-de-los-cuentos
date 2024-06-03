61ce241fa3e280dde723b90297ccfe22f5c76d79
jjlmoya <juanjo1989@gmail.com>
2024-06-03 17:06:57 +0200

    content: optimize images

--- scripts/redimension.py
+++ scripts/redimension.py
@@ -2,16 +2,12 @@ import os
 from PIL import Image
 
 # Rutas de las carpetas con las imágenes
-carpetas_imagenes = ['../src/assets/stories', '../src/assets/sagas']
+carpetas_imagenes = ['../src/assets/stories']
 
 # Nueva resolución
 nueva_resolucion = (896, 512)
 
 for carpeta in carpetas_imagenes:
-    # Crear una carpeta para guardar las imágenes redimensionadas
-    carpeta_redimensionadas = os.path.join(carpeta, 'redimensionadas')
-    os.makedirs(carpeta_redimensionadas, exist_ok=True)
-
     # Recorrer todas las imágenes en la carpeta
     for nombre_imagen in os.listdir(carpeta):
         if nombre_imagen.endswith(('jpg', 'jpeg', 'png', 'bmp', 'gif')):
@@ -20,10 +16,9 @@ for carpeta in carpetas_imagenes:
             # Abrir la imagen
             with Image.open(ruta_imagen) as img:
                 # Redimensionar la imagen
-                img_redimensionada = img.resize(nueva_resolucion, Image.ANTIALIAS)
+                img_redimensionada = img.resize(nueva_resolucion, Image.Resampling.LANCZOS)
                 
-                # Guardar la imagen redimensionada en la nueva carpeta
-                ruta_guardado = os.path.join(carpeta_redimensionadas, nombre_imagen)
-                img_redimensionada.save(ruta_guardado)
+                # Guardar la imagen redimensionada, reemplazando la original
+                img_redimensionada.save(ruta_imagen)
 
-    print(f'Todas las imágenes de {carpeta} han sido redimensionadas y guardadas en {carpeta_redimensionadas}')
+    print(f'Todas las imágenes de {carpeta} han sido redimensionadas y reemplazadas.')
