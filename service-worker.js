a8ca6e4d0e3981b1c203b126d5991f82b23379db
jjlmoya <jjlmoya@users.noreply.github.com>
2024-06-17 07:49:05 +0000

    deploy: 92c186f98c90299cc00b1ac883e46207ccd44497

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'my-site-cache-v1';
+
+// Intercept fetch requests and serve cached files if available
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+
+                // Clone the request to use it for fetching and caching
+                const fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    response => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response to use it for caching
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                // Check if the request URL contains "/assets/" and cache it
+                                if (event.request.url.includes('/assets/')) {
+                                    cache.put(event.request, responseToCache);
+                                }
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+// Activate the service worker and clean up old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
