60dd618a9e40e861144a08776f73b18ba1eeb88b
jjlmoya <jjlmoya@users.noreply.github.com>
2024-08-11 21:11:03 +0000

    deploy: 9ba97fdd2876f1c902635b3b3ae7e920bb7db74c

---
+++ _astro/Loader-DwCC8IVB.CXp2NrSu.js
@@ -0,0 +1,1 @@
+import{B as o}from"./index-CTPMvAAz.DQ1hVuj_.js";import{o as r,f as t}from"./runtime-dom.esm-bundler.BPZR5SMm.js";const e={},n={fill:"currentColor",d:"M12 2C6.477 2 2 6.477 2 12h2a8 8 0 0 1 8-8V2ZM20 12h2c0 5.523-4.477 10-10 10v-2a8 8 0 0 0 8-8Z"};function a(c,l){return r(),t("path",n)}const p=o(e,[["render",a]]);export{p as default};
---
+++ _astro/NewsLettersPage.BjgnNtOo.js
@@ -0,0 +1,144 @@
+import{_ as u,E as f,z as q,a as y}from"./index-CTPMvAAz.DQ1hVuj_.js";/* empty css                             */import{o as r,f as i,a as t,w as l,b as h,t as c,F as b,j as _,c as g}from"./runtime-dom.esm-bundler.BPZR5SMm.js";const L={__name:"NewsLetters",props:{newsletter:{type:Object,default:{}}},setup(n,{expose:o}){o();const e={get VLink(){return f}};return Object.defineProperty(e,"__isScriptSetup",{enumerable:!1,value:!0}),e}},z=["href"];function w(n,o,e,a,s,d){return r(),i("a",{class:"newsletter-card",href:`/newsletter/${e.newsletter.id}/`},[t(a.VLink,{class:"newsletter-card__element"},{default:l(()=>[h("#"+c(e.newsletter.id)+" - "+c(e.newsletter.title),1)]),_:1})],8,z)}const j=u(L,[["render",w],["__scopeId","data-v-7b185cdf"]]),A=[{id:"1",title:"News Audio Letters #1",html:`<p>¡Hola! Bienvenidos a todos a la primera edición de las "News Audio Letters". Muchos de vosotros ya conocéis nuestra\r
+    "Newsletter" semanal, donde resumíamos los acontecimientos de la semana. Hoy queremos hacer algo más moderno y\r
+    cercano. Al igual que narramos cuentos, hemos decidido narrar esta newsletter, convirtiéndola en una especie de\r
+    podcast. Así que, sin más preámbulos, aquí comienza la primera edición de la Newsletter Sonora mensual de La Voz de\r
+    los Cuentos.</p>\r
+\r
+<h2>Introducción</h2>\r
+<p>Como es nuestra primera edición, queremos empezar explicando qué es "La Voz de los Cuentos" para nosotros. Sabemos\r
+    que muchos de vosotros, padres, madres, docentes e incluso los pequeños de la casa, disfrutáis tanto de leer como de\r
+    escuchar historias. Leer es una actividad increíblemente valiosa, no solo por su contenido, sino también por su\r
+    forma. De pequeños, aprendemos por imitación y somos como esponjas.</p>\r
+<p>Así que nos preguntamos, ¿por qué no acercar una narrativa más adulta y entretenida a los niños? ¿Por qué no\r
+    ofrecerles historias con un trasfondo rico y lleno de vida, que no solo enseñen sino que también entretengan? Así\r
+    nació "La Voz de los Cuentos". En nuestros cuentos, puede haber princesas o dragones, y también podemos enseñar a\r
+    los niños la importancia de irse a dormir temprano o de comer verduras para tener una alimentación saludable. Todo\r
+    esto lo hacemos a través del entretenimiento.</p>\r
+<p>Ahora, os vamos a explicar la motivación detrás de algunas de las sagas que tenemos en marcha.</p>\r
+\r
+<h2>Sagas</h2>\r
+<h3>Luna</h3>\r
+<p>Comenzaremos con la <strong>Saga de Luna</strong>. Luna es una joven curiosa que sueña con ser científica. Con una\r
+    mente analítica y un carisma desbordante, sus historias introducen conceptos de física de una manera fácil de\r
+    entender, animando a los más pequeños a analizar el mundo que les rodea. Hablamos de temas como la presión, la\r
+    temperatura, los arcoíris que aparecen en el cielo cuando llueve o por qué vuela un avión de papel. Esta saga es\r
+    recurrente y nunca tendrá fin, porque la ciencia está en todos lados y siempre seguiremos aprendiendo de ella.</p>\r
+\r
+<h3>Eloy</h3>\r
+<p>Pero no solo la ciencia y la rigurosidad científica son importantes, y eso nos lo demuestra <strong>Eloy</strong>.\r
+    Aquí me pondré un poco sentimental y romperé la barrera como narrador para confesaros que Eloy era mi abuelo. Murió\r
+    cuando yo era pequeño y decidí hacerle un homenaje de esta manera. Si habéis leído algún cuento de esta saga,\r
+    sabréis que Eloy se reúne cada noche con sus nietos, frente a la chimenea, para contarles historias que vivió de\r
+    joven. ¿Qué tan viejo será Eloy? No lo sabemos, pero sabemos que vivió con los egipcios, así que debe tener unos\r
+    cuantos miles de años.</p>\r
+<p>La <strong>Saga de Eloy</strong> mezcla humor absurdo e imaginativo con rigor histórico. Los personajes y las\r
+    civilizaciones que ahí aparecen existieron realmente. Esta saga añade magia a hechos del pasado, descubriendo la\r
+    historia de la humanidad desde la perspectiva divertida y fantástica de un abuelo que quiere hacer feliz a sus\r
+    nietos.</p>\r
+\r
+<h3>SDG</h3>\r
+<p>¿A quién no le gustan los gatos? Cada gato merece un hogar, y eso es a lo que se dedica el <strong>Sistema de\r
+        Distribución de Gatos (SDG)</strong>. Esta saga trae el estilo de novela negra y detectivesca a los más\r
+    pequeños. Aunque es un género complicado para ellos, nos gusta el resultado.</p>\r
+<p>Tenemos muchas más sagas, pero por ahora creemos que el concepto queda claro con estos ejemplos.</p>\r
+\r
+<h2>La Página Web</h2>\r
+<p>Aunque comenzamos en YouTube, Spotify y TikTok, nos dimos cuenta de que no era suficiente para todo lo que queríamos\r
+    ofrecer. Así que decidimos invertir esfuerzo en crear la página web: <a\r
+        href="http://lavozdeloscuentos.es">lavozdeloscuentos.es</a>. En ella, podemos agrupar mejor el contenido y las\r
+    sagas, además de ofrecer más funcionalidades para vosotros y expandir las historias para que cobren vida más allá\r
+    del relato. Aquí van algunos ejemplos:</p>\r
+<ul>\r
+    <li>Tenéis la opción de leer el cuento, leerlo mientras lo escucháis, e incluso descargarlo en PDF si lo preferís.\r
+    </li>\r
+    <li>Podéis adaptar el tamaño de la fuente al que prefiráis, recordándolo para futuras ocasiones.</li>\r
+    <li>Además, podéis pedirnos cuentos personalizados. Si queréis regalar un cuento o simplemente ponernos a prueba,\r
+        aceptamos el reto y trabajamos en ello con gusto. Nos gusta darle una vuelta inesperada a cada petición.</li>\r
+</ul>\r
+<p>Y además, tenemos un Road Map para expandir más la página. Aquí os hago un breve resumen:</p>\r
+<ul>\r
+    <li>Haremos un rediseño más amigable.</li>\r
+    <li>Añadiremos juegos educativos y entretenidos basados en las sagas que estamos creando. Ya hemos tenemos uno y\r
+        estamos probándolo antes de lanzarlo.</li>\r
+    <li>Progreso de lectura y recompensas por leer.</li>\r
+    <li>Marcar los cuentos que te gustan para nunca perderlos de vista.</li>\r
+    <li>Una opción para facilitarte el compartir cuentos con quien quieras.</li>\r
+    <li>Seguimiento de Sagas.</li>\r
+    <li>Fichas de personajes, para que se conozcan los personajes fuera de las historias, su personalidad, sus\r
+        diferentes facetas.</li>\r
+    <li>Historias para colorear.</li>\r
+    <li>Actividades específicas de sagas, como votar qué historia queréis que cuente Eloy.</li>\r
+    <li>Aplicaciones para móviles para una mayor comodidad.</li>\r
+</ul>\r
+<p>Y muchísimas más cosas que irán viniendo con el tiempo.</p>\r
+<p>¡Esperamos que disfrutéis de esta nueva forma de estar en contacto con nosotros y con los cuentos que tanto nos\r
+    apasionan!</p>\r
+\r
+<h2>A los cuentos</h2>\r
+<p>Y dada esta pequeña gran introducción inicial. Empezamos ahora sí contando lo que nos ha traído este mes de cuentos y\r
+    magia.</p>\r
+\r
+<h3>SDG</h3>\r
+<p>En el sistema de distribución de Gatos hemos sacado cuatro cuentos de gatos maravillosos: <a\r
+        href="https://lavozdeloscuentos.es/cuento/el-arte-secreto-de-la-gata-mia/">La gata artista Mía</a>, <a\r
+        href="https://lavozdeloscuentos.es/cuento/la-banda-de-los-gatos-musicos/">La Banda de los Gatos Músicos</a>, un\r
+    pequeño homenaje a los 4 Músicos de Bremen. <a\r
+        href="https://lavozdeloscuentos.es/cuento/el-tesoro-de-barbai-el-gato-pirata/">El Tesoro de Barba, el Gato\r
+        Pirata</a>, que pese a ser un pirata descubre que lo más importante es la familia, y la historia de una gata que\r
+    pasa demasiado desapercibida, nuestra <a\r
+        href="https://lavozdeloscuentos.es/cuento/la-desaparicion-de-lunai-la-gata-invisible/">Luna, la gata\r
+        invisible</a>.</p>\r
+\r
+<h3>Carlota Cucaracha</h3>\r
+<p>Por otro lado, Carlota nos ha presentado a su hermana, una experta narradora de historias, que nos habla desde un\r
+    pasado lejano en <a href="https://lavozdeloscuentos.es/cuento/la-gran-aventura-de-zetsy-y-su-pata-perdida/">La Gran\r
+        Aventura de Zetsy y su Pata Perdida</a>, y además Carlota consigue pacificar un momento de tensión en el jardín\r
+    resolviendo <a href="https://lavozdeloscuentos.es/cuento/el-debate-del-monton-de-hojas/">El debate del Montón de\r
+        Hojas</a>.</p>\r
+\r
+<h3>Leyendas</h3>\r
+<ul>\r
+    <li><a href="https://lavozdeloscuentos.es/cuento/la-leyenda-del-gnomo-malhumorado/">La Leyenda del Gnomo\r
+            Malhumorado</a></li>\r
+    <li><a href="https://lavozdeloscuentos.es/cuento/la-leyenda-del-esqueleto-bailarin/">La Leyenda del Esqueleto\r
+            Bailarín</a></li>\r
+</ul>\r
+\r
+<h2>La Vida de Eloy</h2>\r
+<p>Este mes ha sido el nacimiento de la saga histórica del abuelo Eloy, hemos hecho un esfuerzo extra para crear la\r
+    estructura y narrativa, así que la saga con más cuentos ha sido esta.</p>\r
+<p>Eloy ha estado ya en Egipto con faraones, construyendo la Gran Muralla China, en una travesía alpina, y en la Rusia\r
+    de los zares. No nos olvidamos de su tiempo con los vikingos, ni tampoco de cuando estuvo luchando con gladiadores.\r
+    Y, por supuesto, cuando viajó a las Américas: aprendió a bailar tango en Argentina, descubrió cómo usar el\r
+    calendario azteca y estuvo en una ciudad de oro inca.</p>\r
+<p>Seguro que aún le quedan muchas historias que contarnos.</p>\r
+\r
+<h3>Luna y la Física</h3>\r
+<p>Y como cada mes, queremos aportar nuestro granito de ciencia. Luna se ha metido con varios temas básicos:</p>\r
+<ul>\r
+    <li><a href="https://lavozdeloscuentos.es/cuento/luna-y-el-secreto-del-calor/">Luna y el Secreto del Calor</a>:\r
+        Donde aprende cómo el calor funciona.</li>\r
+    <li><a href="https://lavozdeloscuentos.es/cuento/luna-y-el-centro-de-gravedad/">Luna y el Centro de Gravedad</a>:\r
+        Para saber por qué nos caemos y cómo evitarlo, además de poder hacer equilibrios increíbles.</li>\r
+    <li><a href="https://lavozdeloscuentos.es/cuento/luna-y-la-presion/">Luna y la Presión</a>: Luna intenta acercar el\r
+        concepto de presión a través de globos y experimentos.</li>\r
+    <li><a href="https://lavozdeloscuentos.es/cuento/luna-y-la-aventura-del-sonido/">Luna y la Aventura del Sonido</a>:\r
+        ¿Cómo viaja y funciona el sonido? ¿Qué es?</li>\r
+    <li><a href="https://lavozdeloscuentos.es/cuento/luna-y-el-misterio-del-vuelo/">Luna y el Misterio del Vuelo</a>:\r
+        Por qué vuelan y cómo podrían mejorar mis aviones de papel, un poco de aerodinámica básica.</li>\r
+</ul>\r
+\r
+<h3>Llamarada</h3>\r
+<p>En <a href="https://lavozdeloscuentos.es/cuento/la-niebla-del-olvido/">La Niebla del Olvido</a> Llamarada vuelve a\r
+    salvar Imaginaria.</p>\r
+\r
+<h3>Tupi</h3>\r
+<p>Hemos empezado una saga de 20 capítulos de duración de un mago en miniatura que tiene problemas muy serios con su\r
+    sombrero. El pequeño Tupi ya nos ha dejado 2 capítulos por delante, de enfados y magia.</p>\r
+\r
+<h3>Otros cuentos sueltos</h3>\r
+<p>Pero no todos son sagas, a veces simplemente queremos hacer pequeños sueltos. Queríamos hacer un pequeño homenaje a\r
+    una especie local de dinosaurio que se encontró en la Serra de Tramuntana, en Mallorca, y decidimos inventarnos una\r
+    historia del <a href="https://lavozdeloscuentos.es/cuento/el-enigma-del-tramuntanasaurio/">Tramuntanasaurio</a>.</p>\r
+<p>Y hay algunos más que os animamos a descubrirlos, a todos les tenemos un gran cariño.</p>\r
+\r`,spotify:"News-Audio-Letters-1-e2mi7no/a-abf41te"}];function p(){return{getAll:()=>A}}const E={__name:"NewsLetters",setup(n,{expose:o}){o();const{getAll:e}=p(),a=e(),s={getAll:e,newsletters:a,NewsLettersCards:j,get useNewsLetters(){return p}};return Object.defineProperty(s,"__isScriptSetup",{enumerable:!1,value:!0}),s}},S={class:"sections-newsletters"};function N(n,o,e,a,s,d){return r(),i("div",S,[(r(!0),i(b,null,_(a.newsletters,v=>(r(),g(a.NewsLettersCards,{newsletter:v},null,8,["newsletter"]))),256))])}const m=u(E,[["render",N],["__scopeId","data-v-8e7f3584"]]),x={__name:"NewsLettersPage",setup(n,{expose:o}){o();const e={SectionsDefault:m,get VText(){return q},get VContainer(){return y},NewsLetters:m};return Object.defineProperty(e,"__isScriptSetup",{enumerable:!1,value:!0}),e}};function C(n,o,e,a,s,d){return r(),g(a.VContainer,{size:"xl",class:"newsletters-page"},{default:l(()=>[t(a.VText,{class:"newsletters-page__title",as:"h1",variant:"header"},{default:l(()=>[h("News Audio Letters")]),_:1}),t(a.SectionsDefault,null,{default:l(()=>[t(a.NewsLetters)]),_:1})]),_:1})}const M=u(x,[["render",C]]);export{M as default};
