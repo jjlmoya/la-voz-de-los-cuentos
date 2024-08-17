c8a4be6eb0fcf5186b7ae9ca6bf4408b68814955
jjlmoya <jjlmoya@users.noreply.github.com>
2024-08-17 23:51:00 +0000

    deploy: b89dc273cef2c7a571162abbbd219d2aabe07de0

--- _astro/Loader-DwCC8IVB.ClZc5oR9.js
+++
@@ -1,1 +0,0 @@
-import{B as o}from"./_plugin-vue_export-helper.ChID8SNd.js";import{o as r,a as t}from"./runtime-dom.esm-bundler.CAy9xaqX.js";const e={},a={fill:"currentColor",d:"M12 2C6.477 2 2 6.477 2 12h2a8 8 0 0 1 8-8V2ZM20 12h2c0 5.523-4.477 10-10 10v-2a8 8 0 0 0 8-8Z"};function n(c,l){return r(),t("path",a)}const p=o(e,[["render",n]]);export{p as default};
