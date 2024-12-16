0bccc063f5f5850d712df4c070fc64f6b355b8f7
jjlmoya <juanjo1989@gmail.com>
2024-12-16 20:53:13 +0100

    content: update translations

--- src/translations/es/characters.js
+++ src/translations/es/characters.js
@@ -1,6 +1,6 @@
 export default {
   'page.characters.h1':
-    'Personajes mágicos de La Voz De Los Cuentos | Aventuras únicas para soñar',
+    'Personajes mágicos de La Voz De Los Cuentos',
   'page.characters.description':
     'Sumérgete en el universo de "La Voz De Los Cuentos" y descubre personajes inolvidables, cada uno con su propia esencia, rebosante de magia, aventura y singularidad. Desde valientes héroes que enarbolan la esperanza hasta seres fantásticos y enigmáticos que cautivan la imaginación, este reino literario invita a niños y amantes de lo maravilloso a explorar sagas legendarias y encender la chispa de sus más profundos sueños',
   'page.character.personality': 'Personalidad',
