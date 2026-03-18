59d7d4d6b96e6663d0d30b724e7a4cbb2d22bb6f
jjlmoya <juanjo1989@gmail.com>
2026-03-18 21:22:02 +0100

    feat(blog): add divorce and parental separation blog posts (es/en) - long-tail keyword targeting

--- src/data/posts/index.ts
+++ src/data/posts/index.ts
@@ -25,6 +25,7 @@ import { post as celosEs } from './es/celos-hermanos-rivalidad-infantil.ts'
 import { post as rechazoEsEs } from './es/nino-rechaza-escuela-cada-manana.ts'
 import { post as bullyingEs } from './es/bullying-acoso-escolar-cuentos-ayuda.ts'
 import { post as perfeccionismoEs } from './es/perfeccionismo-infantil-cuentos-alivio.ts'
+import { post as divorcionEs } from './es/divorcio-separacion-padres-cuentos.ts'
 
 // English posts
 import { post as howToTellEn } from './en/how-to-tell-stories-to-children.ts'
@@ -53,6 +54,7 @@ import { post as celosEn } from './en/sibling-jealousy-rivalry-stories.ts'
 import { post as rechazoEn } from './en/child-refuses-school-every-morning.ts'
 import { post as bullyingEn } from './en/bullying-school-harassment-stories-help.ts'
 import { post as perfeccionismoEn } from './en/childhood-perfectionism-stories-relief.ts'
+import { post as divorcionEn } from './en/divorce-parental-separation-stories.ts'
 
-export const postsEs = [comoContarEs, beneficiosEs, miiedosEs, edadesEs, dormirEs, creatividadEs, valoresEs, concentracionEs, frustracionEs, miedoFallarEs, autoestimaEs, identidadEs, pacienciaEs, autonomiaEs, rutinaEs, escuelaEs, conflictosEs, soledadEs, rechazaLeerEs, rabiaEs, pesadillasEs, timidezEs, celosEs, rechazoEsEs, bullyingEs, perfeccionismoEs]
-export const postsEn = [howToTellEn, benefitsEn, fearsEn, ageGuideEn, sleepEn, creativityEn, valuesEn, concentrationEn, frustrationEn, fearFailureEn, selfEsteemEn, identidadEn, pacienciaEn, autonomiaEn, rutinaEn, escuelaEn, conflictosEn, soledadEn, rechazaLeerEn, rabiaEn, pesadillasEn, timidezEn, celosEn, rechazoEn, bullyingEn, perfeccionismoEn]
+export const postsEs = [comoContarEs, beneficiosEs, miiedosEs, edadesEs, dormirEs, creatividadEs, valoresEs, concentracionEs, frustracionEs, miedoFallarEs, autoestimaEs, identidadEs, pacienciaEs, autonomiaEs, rutinaEs, escuelaEs, conflictosEs, soledadEs, rechazaLeerEs, rabiaEs, pesadillasEs, timidezEs, celosEs, rechazoEsEs, bullyingEs, perfeccionismoEs, divorcionEs]
+export const postsEn = [howToTellEn, benefitsEn, fearsEn, ageGuideEn, sleepEn, creativityEn, valuesEn, concentrationEn, frustrationEn, fearFailureEn, selfEsteemEn, identidadEn, pacienciaEn, autonomiaEn, rutinaEn, escuelaEn, conflictosEn, soledadEn, rechazaLeerEn, rabiaEn, pesadillasEn, timidezEn, celosEn, rechazoEn, bullyingEn, perfeccionismoEn, divorcionEn]
